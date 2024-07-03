6fc59de4d1412917b08fa70455b063c201d8efeb
NewSouthWave <tigers0110@naver.com>
2024-07-03 19:06:30 +0900

    Initial Commit

---
+++ SilicaGelEcoFan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SilicaGelEcoFan
+//
+//  Created by Nam on 2024/07/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
